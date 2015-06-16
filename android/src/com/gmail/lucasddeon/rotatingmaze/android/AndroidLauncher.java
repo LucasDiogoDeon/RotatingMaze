7edc48088a2ccd6d8b5b96a242126552fcac9efb
Lucas Diogo Deon <lucasddeon@gmail.com>
2015-06-16 15:43:04 -0300

    .

--- android/src/com/gmail/lucasddeon/rotatingmaze/android/AndroidLauncher.java
+++ android/src/com/gmail/lucasddeon/rotatingmaze/android/AndroidLauncher.java
@@ -1,7 +1,9 @@
 package com.gmail.lucasddeon.rotatingmaze.android;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import android.view.WindowManager;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.gmail.lucasddeon.rotatingmaze.RotatingMazeGame;
@@ -10,7 +12,14 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
 		initialize(new RotatingMazeGame(), config);
 	}
 }
